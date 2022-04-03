886fcbae6bb07da8e80b38d46adb0f89b80c5eac
Jantero93 <janzku1993@hotmail.com>
2022-04-03 13:33:59 +0300

    Refactor: Downgrade to bootstrap 5 -> 4. Migrate

--- web-app/src/components/ListItemModal.vue
+++ web-app/src/components/ListItemModal.vue
@@ -8,68 +8,40 @@
     size="lg"
     @ok="submitClicked"
   >
-    <b-container>
-      <b-form>
-        <b-row>
-          <b-form-group>
-            <label>Name</label>
-            <b-form-input id="input-name"></b-form-input>
-          </b-form-group>
-        </b-row>
+    <b-form>
+      <b-form-group>
+        <label>Name</label>
+        <b-form-input id="input-name"></b-form-input>
+      </b-form-group>
 
-        <b-row class="d-flex align-items-baseline pt-2">
-          <b-col>
-            <b-form-group>
-              <label>Price</label>
-              <b-form-input id="input-price"></b-form-input>
-            </b-form-group>
-          </b-col>
+      <b-form-group>
+        <label>Price</label>
+        <b-form-input id="input-price" type="number"></b-form-input>
+      </b-form-group>
 
-          <b-col class="d-flex flex-column align-items-center">
-            <b-form-group v-slot="{ ariaDescribedby }" label="Condition">
-              <b-form-radio-group
-                v-model="input.condition"
-                :aria-describedby="ariaDescribedby"
-              >
-                <b-form-radio value="New">New</b-form-radio>
-                <b-form-radio value="Good">Good</b-form-radio>
-                <b-form-radio value="Fair">Fair</b-form-radio>
-                <b-form-radio value="Poor">Poor </b-form-radio>
-              </b-form-radio-group>
-            </b-form-group>
-          </b-col>
-        </b-row>
+      <b-form-group>
+        <label>Condition</label>
+        <b-form-select v-model="input.condition" class="mb-3">
+          <b-form-select-option value="New">New</b-form-select-option>
+          <b-form-select-option value="Good">Good</b-form-select-option>
+          <b-form-select-option value="Fair">Fair</b-form-select-option>
+          <b-form-select-option value="Bad">Bad</b-form-select-option>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group>
+        <label>Description</label>
+        <b-form-textarea id="input-description" rows="5"></b-form-textarea>
+      </b-form-group>
 
-        <b-row>
-          <b-col>
-            <b-form-group>
-              <label>Description</label>
-              <b-form-textarea
-                id="input-description"
-                rows="5"
-              ></b-form-textarea>
-            </b-form-group>
-          </b-col>
-        </b-row>
-
-        <b-row class="mt-2">
-          <b-form-group>
-            <b-row>
-              <label>Pictures</label>
-            </b-row>
-            <b-row>
-              <b-form-file
-                id="file-input"
-                v-model="input.files"
-                accept="image/*"
-                multiple
-                plain
-              />
-            </b-row>
-          </b-form-group>
-        </b-row>
-      </b-form>
-    </b-container>
+      <b-form-group label="Photos">
+        <b-form-file
+          id="file-default"
+          v-model="input.files"
+          accept="image/jpeg, image/png, image/gif"
+          multiple
+        ></b-form-file>
+      </b-form-group>
+    </b-form>
   </b-modal>
 </template>
 
--- web-app/src/components/NavBar.vue
+++ web-app/src/components/NavBar.vue
@@ -3,22 +3,22 @@
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="d-flex flex-row">
+      <b-navbar-nav class="mr-auto">
         <b-link to="/">Home</b-link>
       </b-navbar-nav>
 
-      <b-navbar-nav class="ms-auto d-flex flex-row">
-        <b-button v-b-modal.modal-lg size="md" variant="success"
-          >List Item</b-button
-        >
-        <b-form-input class="ms-5" placeholder="Search" />
-        <b-button class="ms-2" size="md" variant="outline-info" type="submit"
+      <b-navbar-nav class="mx-auto search-controls">
+        <b-form-input class="ml-5" placeholder="Search" />
+        <b-button class="ml-2" size="md" variant="outline-info" type="submit"
           >Search</b-button
         >
       </b-navbar-nav>
 
-      <b-navbar-nav class="ms-auto d-flex flex-row">
-        <b-link to="my-ebay">My eBay</b-link>
+      <b-navbar-nav id="my-ebay" class="ml-auto">
+        <b-nav-item-dropdown text="my eBay">
+          <b-dropdown-item v-b-modal.modal-lg>List Item</b-dropdown-item>
+          <b-dropdown-item href="#">Sign in</b-dropdown-item>
+        </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
     <ListItemModal />
@@ -37,16 +37,19 @@ export default Vue.extend({
 
 <style scoped>
 .navigation-bar {
-  font-size: 0.8em;
+  font-size: 1em;
   font-family: Arial, Helvetica, sans-serif;
-}
-a {
-  color: gray;
-  text-decoration: none;
 }
 
 a:hover {
   color: #42b983;
+}
+
+a {
+  color: #42b983;
+  text-decoration: none;
+  font-weight: bold;
+  color: #2c3e50;
 }
 
 button {
@@ -54,11 +57,21 @@ button {
   text-align: center;
 }
 
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
 @media screen and (max-width: 575px) {
-  a {
+  a,
+  #my-ebay {
     margin-top: 1em;
     margin-bottom: 1em;
     font-size: 2em;
+  }
+
+  .search-controls {
+    display: flex;
+    flex-direction: row;
   }
 }
 </style>
